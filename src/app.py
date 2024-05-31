6f0a34a8a8325b11fef2b008d17842422df2a371
kenaioz <silvio.cesarrx@hotmail.com>
2024-05-31 13:38:08 -0300

    exceptions

--- src/app.py
+++ src/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, json
+from werkzeug.exceptions import HTTPException
+
 from src.routes.pong import ping
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(ping)
+    app.url_map.strict_slashes = False
+
+    blueprints = [ping]
+
+    @app.errorhandler(HTTPException)
+    def handle_exception(e):
+        response = e.get_response()
+        response.data = json.dumps(
+            {
+                "error": {
+                    "code": e.code,
+                    "name": e.name,
+                    "description": e.description,
+                }
+            }
+        )
+        print(response.data)
+        response.content_type = "application/json"
+        return response
+
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
+
     return app
 
 
